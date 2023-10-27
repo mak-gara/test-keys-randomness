c5c4a31da0ec69c812c275422bc57a3f7ec6559d
mak-gara <maksimgaragulia@gmail.com>
2023-10-27 10:44:58 +0300

    feat: add test for method that checks max series length

--- tests.py
+++ tests.py
@@ -6,13 +6,20 @@ from key_randomness import KeyRandomness
 
 class TestKeyRandomness(unittest.TestCase):
 
-    def test_monobit(self):
-        random_sequences = (generate_random_sequence("01", 20000) for _ in range(5))
+    def setUp(self):
+        self.random_sequences = (generate_random_sequence("01", 20000) for _ in range(5))
 
-        for sequence in random_sequences:
+    def test_monobit(self):
+        for sequence in self.random_sequences:
             with self.subTest(sequence=sequence):
                 key_randomness = KeyRandomness(sequence)
                 self.assertTrue(key_randomness.monobit())
+
+    def test_check_max_series_length(self):
+        for sequence in self.random_sequences:
+            with self.subTest(sequence=sequence):
+                key_randomness = KeyRandomness(sequence)
+                self.assertTrue(key_randomness.check_max_series_length())
 
 
 if __name__ == '__main__':
