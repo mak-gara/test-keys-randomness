0dbd63468f6337c1ea656eabfd0ad0049de66d30
mak-gara <maksimgaragulia@gmail.com>
2023-10-27 11:01:36 +0300

    refactor: remove redundancy in tests

--- tests.py
+++ tests.py
@@ -9,17 +9,19 @@ class TestKeyRandomness(unittest.TestCase):
     def setUp(self):
         self.random_sequences = (generate_random_sequence("01", 20000) for _ in range(5))
 
-    def test_monobit(self):
+    @unittest.skip
+    def test_key_randomness(self, test_function_name):
         for sequence in self.random_sequences:
             with self.subTest(sequence=sequence):
                 key_randomness = KeyRandomness(sequence)
-                self.assertTrue(key_randomness.monobit())
+                test_function = getattr(key_randomness, test_function_name)
+                self.assertTrue(test_function())
+
+    def test_monobit(self):
+        self.test_key_randomness("monobit")
 
     def test_check_max_series_length(self):
-        for sequence in self.random_sequences:
-            with self.subTest(sequence=sequence):
-                key_randomness = KeyRandomness(sequence)
-                self.assertTrue(key_randomness.check_max_series_length())
+        self.test_key_randomness("check_max_series_length")
 
 
 if __name__ == '__main__':
